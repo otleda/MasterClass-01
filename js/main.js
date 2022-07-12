5ec8f7946e15bd6bcca38a2e664a148c188a75a1
Adelto Oliveira Almeida <contato.adtodeveloper@gmail.com>
2022-07-12 01:18:51 -0300

    add arquivo js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
-// play notes
-// keyboards notes
+// GET KEY ALL
+const keys = document.querySelectorAll('.key')
+
+// PLAY NOTES
+
+// KEYBOARDS TYPE
+window.addEventListener('keydown', event => {
+
+    // keycodejs/main.js
+    // typed key pressed
+    // if key exist
+    // play audio
+    console.log(event)
+})
+
 // click with mouse
